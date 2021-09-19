71605721494ebc26ccd4b55af4859e32497195a7
Ramhari Gyawali <ramhari.gyawali@verisk.com>
2021-09-19 11:38:49 +0545

    authorize attribute

---
+++ src/Application/Common/Security/AuthorizeAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Application.Common.Security
+{
+    /// <summary>
+    /// Specifies the class this attribute is applied to requires authorization.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    public class AuthorizeAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AuthorizeAttribute"/> class. 
+        /// </summary>
+        public AuthorizeAttribute() { }
+
+    }
+}
