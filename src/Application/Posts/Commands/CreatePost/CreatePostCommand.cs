ab14a282002c4d50d4390ce3072bd395c5b334e3
Ramhari Gyawali <ramhari.gyawali@verisk.com>
2021-09-19 13:46:48 +0545

    add post commands

---
+++ src/Application/Posts/Commands/CreatePost/CreatePostCommand.cs
@@ -0,0 +1,41 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Security;
+using Domain.Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Posts.Commands.CreatePost
+{
+    [Authorize]
+    public class CreatePostCommand : IRequest<int>
+    {
+        public string Caption { get; internal set; }
+        public string Image { get; internal set; }
+    }
+    public class CreatePostCommandHandler : IRequestHandler<CreatePostCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IDateTime _dateTime;
+
+        public CreatePostCommandHandler(IApplicationDbContext context, ICurrentUserService currentUserService, IDateTime dateTime)
+        {
+            _context = context;
+            _currentUserService = currentUserService;
+            _dateTime = dateTime;
+        }
+        public async Task<int> Handle(CreatePostCommand request, CancellationToken cancellationToken)
+        {
+            var post = new Post
+            {
+                Caption = request.Caption,
+                CreatedAt = _dateTime.Now,
+                CreatorId = _currentUserService.AccountId,
+                Image = request.Image,
+            };
+            _context.Posts.Add(post);
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/Application/Posts/Commands/CreatePost/CreatePostCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Posts.Commands.CreatePost
+{
+    public class CreatePostCommandValidator : AbstractValidator<CreatePostCommand>
+    {
+        public CreatePostCommandValidator()
+        {
+            RuleFor(v => v.Caption)
+                .NotNull()
+                .NotEmpty();            
+            RuleFor(v => v.Image)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+}
