d17e2c045ae40aa067965e02dd37feb5586d823e
Ramhari Gyawali <ramhari.gyawali@verisk.com>
2021-09-19 11:35:56 +0545

    add interfaces

---
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Account> Accounts { get; set; }
+        DbSet<Post> Posts { get; set; }
+
+        DbSet<Comment> Comments { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
