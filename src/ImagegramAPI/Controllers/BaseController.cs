c7d928c0f393a800b66a5df44365587b6b906aef
Ramhari Gyawali <ramhari.gyawali@verisk.com>
2021-09-19 14:07:07 +0545

    add lambda API project

---
+++ src/ImagegramAPI/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace ImagegramAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public abstract class BaseController : ControllerBase
+    {
+        private ISender _mediator;
+
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetService<ISender>();
+    }
+}
